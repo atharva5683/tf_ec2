d4ba361c36f94942fddb129b78b5a7a22bd6a06e
Atharva Kumbhar <160429511+atharva5683@users.noreply.github.com>
2024-09-30 02:13:46 +0530

    Update main.tf

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -7,7 +7,7 @@ resource "aws_default_vpc" "default" {
 
 }
 
-resource "aws_security_group" "http_server_sg" {
+resource "aws_security_group" "Instance_sg" {
   name = "http_server_sg"
   //vpc_id = "vpc-c49ff1be"
   vpc_id = aws_default_vpc.default.id
